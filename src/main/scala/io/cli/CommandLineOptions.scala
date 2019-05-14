b2dbe0401090afb0cc5d14dc5c6be1a68d87ecec
Lukas Heumos <lukas.heumos@gmail.com>
2019-05-14 15:59:13 +0200

    [FIX] Removed unnecessary types in commandlineoptions.scala

--- src/main/scala/io/cli/CommandLineOptions.scala
+++ src/main/scala/io/cli/CommandLineOptions.scala
@@ -16,23 +16,23 @@ class CommandLineOptions {
 
   @Option(names = Array("-d", "--driver"),
     description = Array("Database driver name"))
-  var databaseDriver: String = ""
+  var databaseDriver = ""
 
   @Option(names = Array("-c", "--config"),
     description = Array("Database config file path."),
     required = true)
-  var configFilePath: String = ""
+  var configFilePath = ""
 
   @Option(names = Array("-t", "--table"),
     description = Array("Table to run query on. Required if using Spark."))
-  var table: String = ""
+  var table = ""
 
   @Option(names = Array("-q", "--query"),
     description = Array("SQL query to execute."),
     arity = "0..1",
     interactive = true,
     required = true)
-  var sqlQuery: String = ""
+  var sqlQuery = ""
 
   @Option(names = Array("-h", "--help"),
     description = Array("Display a help message."),
